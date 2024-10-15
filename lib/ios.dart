b259745865f07af57c7c78d0f95d93ce47780798
0eta0 <eta.develop@gmail.com>
2024-10-15 13:07:38 +0900

    fix: add double quate

--- lib/ios.dart
+++ lib/ios.dart
@@ -338,7 +338,7 @@ Future<void> changeIosLauncherIcon(String iconName, String? flavor) async {
       if (currentConfig != null &&
           (flavor == null || currentConfig.contains('-$flavor')) &&
           line.contains('ASSETCATALOG')) {
-        lines[x] = line.replaceAll(RegExp('\=(.*);'), '= $iconName;');
+        lines[x] = line.replaceAll(RegExp('\=(.*);'), '= \"$iconName\";');
       }
     }
   }
